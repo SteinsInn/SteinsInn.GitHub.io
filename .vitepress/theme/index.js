3031715f3da379afede812aa9e37069239fe7f77
夜影、NightShadow <169712528+1224HuangJin@users.noreply.github.com>
2026-04-06 15:47:45 +0200

    载入 custom.css 的内容

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import MyLayout from './Layout.vue' // 导入你刚创建的 Layout.vue
+import './custom.css' // 载入 custom.css 的内容
 
 export default {
   extends: DefaultTheme,
